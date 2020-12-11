64f5bf163260f5b725d5fd1b405acce21826ff28
Yves Augusto <53539227+YvesAugusto@users.noreply.github.com>
2020-12-11 07:52:29 -0300

    Update conv_layer.py

--- conv_layer.py
+++ conv_layer.py
@@ -23,25 +23,10 @@ class ConvLayer:
     return X
 
   def copyFromKerasLayers(self, layer):
-    # only 1 layer to copy from
     W, bias = layer.get_weights()
     op1 = self.W.assign(W)
     op2 = self.bias.assign(bias)
     self.session.run((op1, op2))  
     
-  # def copyFromWeights(self, W, b):
-  #   op1 = self.W.assign(W)
-  #   op2 = self.b.assign(b)
-  #   self.session.run((op1, op2))
-
   def get_params(self):
     return [self.W, self.bias]
-
-# init_op = tf.global_variables_initializer()
-# conv=ConvLayer(1, 3, 64, 2)
-# forward=conv.forward(np.random.random((1, 224, 224, 3)))
-# init = tf.global_variables_initializer()
-# with tf.Session() as sess:
-#     sess.run(init)
-#     print(forward.eval().shape)
-#     sess.close()
